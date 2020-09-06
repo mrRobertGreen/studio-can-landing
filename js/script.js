43927a1b241ecf6f386bd47b235b2f34f4d3c0b5
Robert Green <mrrobertgreen17@gmail.com>
2020-09-06 17:39:30 +0300

    work
    + sticky menu
    + msg clouds
    + bg for we-are block

--- js/script.js
+++ js/script.js
@@ -3,4 +3,15 @@ $(document).ready(function(){
       $(".menu__item").removeClass("menu__item_active")
       $(this).addClass("menu__item_active")
    })
+
+   $(window).scroll(function() {
+      if($(this).scrollTop() >= 90) {
+          $('.menu').addClass('stickytop');
+          $('.top').addClass('padding-for-top-block')
+      }
+      else{
+          $('.menu').removeClass('stickytop');
+          $('.top').removeClass('padding-for-top-block')
+      }
+  });
 });
