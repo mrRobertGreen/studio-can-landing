bd4ba07c1878b6bc37b235ac0e0c3dce4c61681b
Robert Green <mrrobertgreen17@gmail.com>
2020-09-10 22:09:59 +0300

    work
    + links

--- js/script.js
+++ js/script.js
@@ -4,23 +4,23 @@ $(document).ready(function(){
         $(this).addClass("menu__item_active")
         const id = $(this).attr('id');
         const fakeItem = $(".menu__item_fake")
-        switch (+id) {
-            case 1:
+        switch (id) {
+            case "item-1":
                 fakeItem.animate({left: "1px"}, 300)
                 break
-            case 2:
+            case "item-2":
                 fakeItem.animate({left: "152px"}, 300)
                 break
-            case 3:
+            case "item-3":
                 fakeItem.animate({left: "303px"}, 300)
                 break
-            case 4:
+            case "item-4":
                 fakeItem.animate({left: "454px"}, 300)
                 break
-            case 5:
+            case "item-5":
                 fakeItem.animate({left: "605px"}, 300)
                 break
-            case 6:
+            case "item-6":
                 fakeItem.animate({left: "756px"}, 300)
                 break
             default: 
@@ -39,4 +39,22 @@ $(document).ready(function(){
         }
     });
 
+    $(".menu__row").on('click', '[href*="#"]', function(e){
+        $.scrollTo($(this.hash), 500, {'offset':-70});
+        e.preventDefault();
+    });
+    
+    // $('.main-menu [href]').each(function() {
+    //     if (this.href == window.location.href) {
+    //       $(this).addClass('active');
+    //     }
+    //   });
+
+
+    // $(window).scroll(function() {
+    //     if (0 < $(this).scrollY() < 300) {
+
+    //         $("#item-1" ).find( ".menu__item" ).trigger('click');
+    //     }
+    // });  
 });
