e49e3189b5dcfffeb9ffb4dcb5756e63eb98f853
Robert Green <mrrobertgreen17@gmail.com>
2020-10-19 21:10:56 +0300

    work
    nav menu

--- js/script.js
+++ js/script.js
@@ -1,56 +1,64 @@
-$(document).ready(function(){
-    $(".menu__item").click(function() {
+$(document).ready(function () {
+    $("#menu-fake").width($("#menu-we").width())
+    $(".menu__item").click(function () {
         // listen menu-item click  
         $(".menu__item").removeClass("menu__item_active")
         $(this).addClass("menu__item_active")
         const id = $(this).attr('id');
-        const fakeItem = $(".menu__item_fake")
+        const fakeItem = $("#menu-fake")
         switch (id) {
             case "menu-we":
-                moveFakeItem(1)
+                moveFakeItem(3)
+                fakeItem.width($(this).width())
                 break
             case "menu-services":
-                moveFakeItem(152)
+                moveFakeItem($("#menu-we").outerWidth() + 3)
+                fakeItem.width($(this).width())
                 break
             case "menu-examples":
-                moveFakeItem(303)
+                moveFakeItem($("#menu-we").outerWidth() + $("#menu-services").outerWidth() + 3)
+                fakeItem.width($(this).width())
                 break
             case "menu-process":
-                moveFakeItem(454)
+                moveFakeItem($("#menu-we").outerWidth() + $("#menu-services").outerWidth() + $("#menu-examples").outerWidth() + 3)
+                fakeItem.width($(this).width())
                 break
             case "menu-vacancies":
-                moveFakeItem(605)
+                moveFakeItem($("#menu-we").outerWidth() + $("#menu-services").outerWidth() + $("#menu-examples").outerWidth() + $("#menu-process").outerWidth() + 3)
+                fakeItem.width($(this).width())
                 break
             case "menu-contacts":
-                moveFakeItem(756)
+                moveFakeItem($("#menu-we").outerWidth() + $("#menu-services").outerWidth() + $("#menu-examples").outerWidth() + $("#menu-process").outerWidth() + $("#menu-vacancies").outerWidth())
+                fakeItem.width($(this).width())
                 break
-            default: 
-                fakeItem.animate({left: "", right: "1px"}, 300)
-    }
+            default:
+                break
+        }
     })
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         // make menu fixed on top
-        if($(this).scrollTop() >= 70) {
+        if ($(this).scrollTop() >= 70) {
             $('.menu').addClass('stickytop');
             $('.top').addClass('p-top-184')
-        }
-        else {
+        } else {
             $('.menu').removeClass('stickytop');
             $('.top').removeClass('p-top-184')
         }
     });
 
-    $(".menu__row").on('click', '[href*="#"]', function(e){
+    $(".menu__row").on('click', '[href*="#"]', function (e) {
         // make smooth scroll to anchor
-        $.scrollTo($(this.hash), 500, {'offset':-70});
+        $.scrollTo($(this.hash), 500, {
+            'offset': -70
+        });
         e.preventDefault();
     });
-    
+
     let visibleElem = null
     // let prevScrollPos = 0
 
-    $(window).on('resize scroll', function() {
+    $(window).on('resize scroll', function () {
         // switch menu items when scrolling
         let pagesArr = [
             "we",
@@ -70,23 +78,37 @@ $(document).ready(function(){
             visibleElem = elem
         }
     });
+
+    // horizontal scroll on menu
+    if (window.matchMedia('(max-width: 768px)').matches) {
+        var options = {
+            horizontal: 1,
+            speed: 300,
+            mouseDragging: 1,
+            touchDragging: 1,
+        };
+        var frame = new Sly('#frame', options).init();
+    }
+
 });
 
-$.fn.isInViewport = function() {  
+$.fn.isInViewport = function () {
     // check element visibility
     const elementTop = $(this).offset().top;
     const elementBottom = elementTop + $(this).outerHeight();
-  
+
     const menuHeight = 70 // height of top fixed menu
 
     const viewportTop = $(window).scrollTop() + menuHeight;
     const viewportBottom = viewportTop + $(window).height();
-  
+
     return elementBottom > viewportTop && elementTop < viewportBottom;
 };
 
 const moveFakeItem = (left = 1, delay = 300) => {
     // move fake menu item to left with delay
     const fakeItem = $(".menu__item_fake")
-    fakeItem.animate({left: `${left}px`}, delay)
+    fakeItem.animate({
+        left: `${left}px`
+    }, delay)
 }
