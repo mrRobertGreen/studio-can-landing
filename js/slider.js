e4f0edf3e704efb84570487d863d407d0d1b67c7
Robert Green <mrrobertgreen17@gmail.com>
2020-11-13 22:08:51 +0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
     $(".slider").slick({
         arrows: false,
         dots: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
     })
- });
+
+    $('.slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+        if (nextSlide === 0) {
+            $(".slider").slick('slickSetOption', 'autoplaySpeed', 2500);
+        } else {
+            $(".slider").slick('slickSetOption', 'autoplaySpeed', 4999);
+        }
+    });
+});
