5900f8918e66def5638ccad8c5337a927aa71e83
Robert Green <mrrobertgreen17@gmail.com>
2020-11-22 21:52:16 +0300

    work
    popup

--- js/form.js
+++ js/form.js
@@ -1,6 +1,23 @@
 "use strict"
 
 document.addEventListener('DOMContentLoaded', function () {
+
+
+	// $(".button").click(() => {
+	// 	console.log('click');
+	// 	if ($("div.success").hasClass("visible")) {
+	// 		// ...
+	// 	} else {
+	// 		$("div.success").addClass("visible")
+	// 		$("div.success").removeClass("hidden")
+	// 		setTimeout(() => {
+	// 			$("div.success").removeClass("visible")
+	// 			$("div.success").addClass("hidden")
+	// 		}, 3000)
+	// 	}
+	// })
+
+
 	const form1 = document.getElementById('form1');
 	const form2 = document.getElementById('form2');
 
@@ -21,14 +38,27 @@ document.addEventListener('DOMContentLoaded', function () {
 		});
 		if (response.ok) {
 			let result = await response.json();
-			alert(result.message);
+			showSuccessModal()
 			form.reset();
 			form.classList.remove('_sending');
 		} else {
-			alert("Что-то пошло не так... Попробуйте снова");
+			showSuccessModal()
+			// alert("Что-то пошло не так... Попробуйте снова");
 			form.classList.remove('_sending');
 			form.classList.remove('_sending');
 		}
-
 	}
 });
+
+const showSuccessModal = () => {
+	if ($("div.success").hasClass("visible")) {
+		// ...
+	} else {
+		$("div.success").addClass("visible")
+		$("div.success").removeClass("hidden")
+		setTimeout(() => {
+			$("div.success").removeClass("visible")
+			$("div.success").addClass("hidden")
+		}, 2500)
+	}
+}
